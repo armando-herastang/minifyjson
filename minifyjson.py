2e0bd4f4a2b057c3587a9c5af41fbcd00a6592ed
Armando Heras Tang <armando@swagup.com>
2020-04-16 19:46:34 -0400

    ADD: Exception handling on FileNotFound

--- minifyjson.py
+++ minifyjson.py
@@ -12,18 +12,23 @@ class JSONMinifier():
         self.input_path = filepath
 
     def minify_sequential(self):
-        with open(self.input_path, 'r') as iFile, open("output.json", 'w') as oFile:
-            insideQuotes = False
-            while(True):
-                c = iFile.read(1)
-                if not c:
-                    break
-                insideQuotes = c == DOUBLE_QUOTE
-                if not insideQuotes:
-                    if c not in REMOVABLE_CHARS:
+        try:
+            with open(self.input_path, 'r') as iFile, open("output.json", 'w') as oFile:
+                insideQuotes = False
+                while(True):
+                    c = iFile.read(1)
+                    if not c:
+                        break
+                    insideQuotes = c == DOUBLE_QUOTE
+                    if not insideQuotes:
+                        if c not in REMOVABLE_CHARS:
+                            oFile.write(c)
+                    else:
                         oFile.write(c)
-                else:
-                    oFile.write(c)
+        except FileNotFoundError as noFileError:
+            print(noFileError)
+
+
 
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser(description="Minify JSON file")
