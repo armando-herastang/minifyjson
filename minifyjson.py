8487cd95b5920dc69a541874d133a788d001e38b
Armando Heras Tang <armando@swagup.com>
2020-04-16 19:40:52 -0400

    ADD: Simple removal of newline/whitespace chars outside double quotes

--- minifyjson.py
+++ minifyjson.py
@@ -1,6 +1,34 @@
 import argparse
+import json
+
+REMOVABLE_CHARS = {
+    '\n' : "New line",
+    ' ': "Whitespace"
+}
+
+DOUBLE_QUOTE = "\""
+class JSONMinifier():
+    def __init__(self, filepath):
+        self.input_path = filepath
+
+    def minify_sequential(self):
+        with open(self.input_path, 'r') as iFile, open("output.json", 'w') as oFile:
+            insideQuotes = False
+            while(True):
+                c = iFile.read(1)
+                if not c:
+                    break
+                insideQuotes = c == DOUBLE_QUOTE
+                if not insideQuotes:
+                    if c not in REMOVABLE_CHARS:
+                        oFile.write(c)
+                else:
+                    oFile.write(c)
 
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser(description="Minify JSON file")
     arg_parser.add_argument(type=str, help="Path to the JSON file", dest="filepath")
     args = arg_parser.parse_args()
+
+    minifier = JSONMinifier(args.filepath)
+    minifier.minify_sequential()
